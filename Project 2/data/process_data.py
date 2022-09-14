94ea4994488764b3f381ae6e7828f3c610049280
Hoang <hoang.th586@gmail.com>
2022-09-14 16:29:45 +0700

    update process_data.py and filepaths

--- Project 2/data/process_data.py
+++ Project 2/data/process_data.py
@@ -67,7 +67,7 @@ def clean_data(df):
     categories['related'] = categories['related'].replace(to_replace=2, value=1)
 
     # drop the original categories column from `df`
-    df = df.drop(['categories'], axis = 1, inplace = True)
+    df.drop(['categories'], axis = 1, inplace = True)
 
     # concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat([df,categories], axis = 1, join = 'inner')
